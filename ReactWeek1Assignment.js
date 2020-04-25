c99376405c40c06000d25d927cd2e7df09dad3e9
Keith Martin <KM@Keiths-MacBook-Air.local>
2020-04-25 14:22:02 -0400

    first commit

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,13 @@
+class Student {
+    constuctor (name,level,community = []) {
+        this.name = name
+        this.level = level
+        this.community = community
+    }
+
+class Bootcamp {
+    constuctor (name,level,student = []) {
+        this.name = name
+        this.level = level
+        this.student = student
+     }
